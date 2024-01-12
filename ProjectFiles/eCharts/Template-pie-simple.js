fe3192c08666b393413d4b479b7132a638bbd1ce
PiyaliKeshRA <piyali.kesh@rockwellautomation.com>
2024-01-12 17:09:01 +0530

    PrimarySaaS

---
+++ ProjectFiles/eCharts/Template-pie-simple.js
@@ -0,0 +1,50 @@
+    var dom = document.getElementById('container');
+    var myChart = echarts.init(dom, 'dark', {
+      renderer: 'canvas',
+      useDirtyRect: false
+    });
+    var app = {};
+    
+    var option;
+
+    option = {
+  title: {
+    text: 'My Plant production',
+    subtext: 'Tentative Data',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: $01, name: 'Total Good production' },
+        { value: $02, name: 'Rejection' },
+        { value: $03, name: 'Performance' },
+        { value: $04, name: 'Availability' },
+        { value: $05, name: 'Quality' }
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+};
+
+    if (option && typeof option === 'object') {
+      myChart.setOption(option);
+    }
+
+    window.addEventListener('resize', myChart.resize);
